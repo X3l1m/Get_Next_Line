3e808910382b7bf31d6017bee3caef739485f596
Selim Yildirim <seyildir@f1r4s7.codam.nl>
2023-02-15 20:30:14 +0100

    codam

--- get_next/get_next_line.h
+++ get_next/get_next_line.h
@@ -6,13 +6,15 @@
 #include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 100
+# define BUFFER_SIZE 20
 #endif
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-char	*ft_strjoin(char const *s1, char const *s2, size_t limit);
-char	*ft_strdup(char *s1);
-
+void	*ft_memcpy(void *dst, const void * src, size_t n);
+char	*ft_strjoin(char  *s1, char const *s2, size_t limit);
+char	*ft_strdup(char const *s1);
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
